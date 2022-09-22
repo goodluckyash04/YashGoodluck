5afc87d45313f3f4dffc9dc0313ba69f67f91249
goodluckyash04 <107934732+goodluckyash04@users.noreply.github.com>
2022-09-22 13:20:17 +0530

    22nd sep

--- task2.1.py
+++ task2.1.py
@@ -1,9 +1,10 @@
 # find the number divisible by which number
 
 x = int(input())
-if(x%2==0):
-    a=x/2
-    if(a%3==0):
-        print('number is divisible by 2,3,6')
-    else:
-        print('number is divisible by 2')
+a = 2
+while a != 11:
+    if x%a == 0 :
+        print('num is divisible by ',a)
+    a+=1
+
+
